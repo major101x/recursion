87dd47d975abe2484e679add1aec33666eefa7dc
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2025-04-21 09:25:02 +0100

    write iterative fibonacci function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+/* Fibonacci function */
+function fibs(n) {
+  let result = [0, 1];
+  let firstNumberIndex = 0;
+  let secondNumberIndex = 1;
+
+  if (n < 2) {
+    return [0];
+  } else if (n === 1) {
+    return [0];
+  }
+
+  while (n > 2) {
+    let i = result[firstNumberIndex] + result[secondNumberIndex];
+
+    result.push(i);
+    firstNumberIndex++;
+    secondNumberIndex++;
+    n--;
+  }
+
+  return result;
+}
+
+console.log(fibs(8));
 console.log("Console working!");
